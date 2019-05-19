499b7c0b5572c354134c4ced71b94032b0361c50
thanhdat285 <dd.tt97@gmail.com>
2019-05-19 16:44:24 +0700

    Draw matches

--- main.py
+++ main.py
@@ -1,55 +1,214 @@
 import cv2
 import os
 import numpy as np
+import math
 if not os.path.isdir('test'):
     os.makedirs('test')
 
+import pygame
+from OpenGL.GL import *
+
+def MTL(filename):
+    contents = {}
+    mtl = None
+    for line in open(filename, "r"):
+        if line.startswith('#'): continue
+        values = line.split()
+        if not values: continue
+        if values[0] == 'newmtl':
+            mtl = contents[values[1]] = {}
+        elif mtl is None:
+            raise (ValueError, "mtl file doesn't start with newmtl stmt")
+        elif values[0] == 'map_Kd':
+            # load the texture referred to by this declaration
+            mtl[values[0]] = values[1]
+            surf = pygame.image.load(mtl['map_Kd'])
+            image = pygame.image.tostring(surf, 'RGBA', 1)
+            ix, iy = surf.get_rect().size
+            texid = mtl['texture_Kd'] = glGenTextures(1)
+            glBindTexture(GL_TEXTURE_2D, texid)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+                GL_LINEAR)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA,
+                GL_UNSIGNED_BYTE, image)
+        else:
+            mtl[values[0]] = map(float, values[1:])
+    return contents
+
+class OBJ:
+    def __init__(self, filename, swapyz=False):
+        """Loads a Wavefront OBJ file. """
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.faces = []
+
+        material = None
+        for line in open(filename, "r"):
+            if line.startswith('#'): continue
+            values = line.split()
+            if not values: continue
+            if values[0] == 'v':
+                v = map(float, values[1:4])
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.vertices.append(v)
+            elif values[0] == 'vn':
+                v = map(float, values[1:4])
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.normals.append(v)
+            elif values[0] == 'vt':
+                self.texcoords.append(map(float, values[1:3]))
+            elif values[0] in ('usemtl', 'usemat'):
+                material = values[1]
+            elif values[0] == 'mtllib':
+                self.mtl = MTL(values[1])
+            elif values[0] == 'f':
+                face = []
+                texcoords = []
+                norms = []
+                for v in values[1:]:
+                    w = v.split('/')
+                    face.append(int(w[0]))
+                    if len(w) >= 2 and len(w[1]) > 0:
+                        texcoords.append(int(w[1]))
+                    else:
+                        texcoords.append(0)
+                    if len(w) >= 3 and len(w[2]) > 0:
+                        norms.append(int(w[2]))
+                    else:
+                        norms.append(0)
+                self.faces.append((face, norms, texcoords, material))
+
+        self.gl_list = glGenLists(1)
+        glNewList(self.gl_list, GL_COMPILE)
+        glEnable(GL_TEXTURE_2D)
+        glFrontFace(GL_CCW)
+        for face in self.faces:
+            vertices, normals, texture_coords, material = face
+
+            mtl = self.mtl[material]
+            if 'texture_Kd' in mtl:
+                # use diffuse texmap
+                glBindTexture(GL_TEXTURE_2D, mtl['texture_Kd'])
+            else:
+                # just use diffuse colour
+                glColor(*mtl['Kd'])
+
+            glBegin(GL_POLYGON)
+            for i in range(len(vertices)):
+                if normals[i] > 0:
+                    glNormal3fv(self.normals[normals[i] - 1])
+                if texture_coords[i] > 0:
+                    glTexCoord2fv(self.texcoords[texture_coords[i] - 1])
+                glVertex3fv(self.vertices[vertices[i] - 1])
+            glEnd()
+        glDisable(GL_TEXTURE_2D)
+        glEndList()
+
+
+def projection_matrix(camera_parameters, homography):
+    """
+    From the camera calibration matrix and the estimated homography
+    compute the 3D projection matrix
+    """
+    # Compute rotation along the x and y axis as well as the translation
+    homography = homography * (-1)
+    rot_and_transl = np.dot(np.linalg.inv(camera_parameters), homography)
+    col_1 = rot_and_transl[:, 0]
+    col_2 = rot_and_transl[:, 1]
+    col_3 = rot_and_transl[:, 2]
+    # normalise vectors
+    l = math.sqrt(np.linalg.norm(col_1, 2) * np.linalg.norm(col_2, 2))
+    rot_1 = col_1 / l
+    rot_2 = col_2 / l
+    translation = col_3 / l
+    # compute the orthonormal basis
+    c = rot_1 + rot_2
+    p = np.cross(rot_1, rot_2)
+    d = np.cross(c, p)
+    rot_1 = np.dot(c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_2 = np.dot(c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_3 = np.cross(rot_1, rot_2)
+    # finally, compute the 3D projection matrix from the model to the current frame
+    projection = np.stack((rot_1, rot_2, rot_3, translation)).T
+    return np.dot(camera_parameters, projection)
+
+def render(img, obj, projection, model, color=False):
+    vertices = obj.vertices
+    scale_matrix = np.eye(3) * 3
+    h, w = model.shape
+
+    for face in obj.faces:
+        face_vertices = face[0]
+        points = np.array([vertices[vertex - 1] for vertex in face_vertices])
+        points = np.dot(points, scale_matrix)
+        # render model in the middle of the reference surface. To do so,
+        # model points must be displaced
+        points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
+        dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
+        imgpts = np.int32(dst)
+        if color is False:
+            cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
+        else:
+            color = hex_to_rgb(face[-1])
+            color = color[::-1] # reverse
+            cv2.fillConvexPoly(img, imgpts, color)
+
+    return img
+
+
+
+obj = OBJ("low-poly-fox-by-pixelmannen.obj", swapyz=True)
+camera_parameters = np.array([[600, 0, 320], [0, 600, 240], [0, 0, 1]])
 MIN_MATCHES = 10
 
-cap = cv2.imread('surface.png', 0)
-img_rgb = cv2.imread('surface.png')
 model = cv2.imread('model.png', 0)
-
-# ORB keypoint detector
-orb = cv2.ORB_create()
+orb = cv2.ORB_create() # ORB keypoint detector
 # create brute force  matcher object
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 # Compute model keypoints and its descriptors
 kp_model, des_model = orb.detectAndCompute(model, None)
-# Compute scene keypoints and its descriptors
-kp_frame, des_frame = orb.detectAndCompute(cap, None)
-# Match frame descriptors with model descriptors
-matches = bf.match(des_model, des_frame)
-# Sort them in the order of their distance
-matches = sorted(matches, key=lambda x: x.distance)
 
-if len(matches) > MIN_MATCHES:
-    # draw first 15 matches.
-    # cap = cv2.drawMatches(model, kp_model, cap, kp_frame,
-    #                       matches[:MIN_MATCHES], 0, flags=2)
-    # # show result
-    # cv2.imshow('frame', cap)
-    # cv2.waitKey(0)
+cam = cv2.VideoCapture(0)
+while True:
+    ret_val, img_rgb = cam.read()
+    cap = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+    # Compute scene keypoints and its descriptors
+    kp_frame, des_frame = orb.detectAndCompute(cap, None)
+    # Match frame descriptors with model descriptors
+    matches = bf.match(des_model, des_frame)
+    # Sort them in the order of their distance
+    matches = sorted(matches, key=lambda x: x.distance)
 
-    # assuming matches stores the matches found and
-    # returned by bf.match(des_model, des_frame)
-    # differenciate between source points and destination points
-    src_pts = np.float32([kp_model[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp_frame[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+    if len(matches) > MIN_MATCHES:
+        # draw first 15 matches.
+        cap = cv2.drawMatches(model, kp_model, cap, kp_frame,
+                              matches[:MIN_MATCHES], 0, flags=2)
+        # # show result
+        # cv2.imshow('frame', cap)
+        # cv2.waitKey(0)
 
-    # compute Homography
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        # assuming matches stores the matches found and
+        # returned by bf.match(des_model, des_frame)
+        # differenciate between source points and destination points
+        src_pts = np.float32([kp_model[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp_frame[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
 
-    # Draw a rectangle that marks the found model in the frame
-    h, w = model.shape
-    pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-    # project corners into frame
-    dst = cv2.perspectiveTransform(pts, M)
-    # connect them with lines
-    img2 = cv2.polylines(img_rgb, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
-    cv2.imshow('frame', img2)
-    cv2.waitKey(0)
+        # compute Homography
+        homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
-else:
-    print("Not enough matches have been found - %d/%d" % (len(matches),
-                                                          MIN_MATCHES))
+        projection = projection_matrix(camera_parameters, homography)
+
+        img_rgb = render(img_rgb, obj, projection, model, False)
+    else:
+        print("Not enough matches have been found - %d/%d" % (len(matches),
+                                                            MIN_MATCHES))
+    cv2.imshow('frame', cap)
+    # cv2.imshow('webcam', img_rgb)
+    if cv2.waitKey(1) == 27: 
+        break  # esc to quit
+cv2.destroyAllWindows()
