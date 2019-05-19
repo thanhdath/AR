d4cbe34cf8615a72ecf3234bed0bc11d3bb0c2ee
hoang dzung <dungmin97@gmail.com>
2019-05-19 23:54:28 +0700

    aruco marker

--- aruco.py
+++ aruco.py
@@ -26,7 +26,8 @@
 import numpy as np
 import cv2
 import cv2.aruco as aruco
-
+import pygame
+import math
 
 cap = cv2.VideoCapture(0)
 import imgaug.augmenters as iaa
@@ -34,10 +35,22 @@ aug = iaa.Sequential([
     iaa.PerspectiveTransform(scale=(0.0, 0.1))
 ])
 import time
+from OpenGL.GL import *
+
+from model import OBJ
+from utils import projection_matrix, render
 
 # camera_matrix = np.array([[800, 0, 320], [0, 600, 240], [0, 0, 1]])
 # dist_coeffs = np.array([1,1,1,1])
-
+obj = OBJ("low-poly-fox-by-pixelmannen.obj", swapyz=True)
+model = np.ones((20, 20))
+src_pts = np.array([[
+    [0,0],
+    [99,0],
+    [99,99],
+    [0,99]
+]])
+camera_parameters = np.array([[600, 0, 320], [0, 600, 240], [0, 0, 1]])
 camera_matrix = np.load('camera_matrix.npy')
 dist_coeffs = np.load('dist_coeff.npy')
 
@@ -62,7 +75,6 @@ while(True):
         '''
         #lists of ids and the corners beloning to each id
     corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-    # print(corners[0].shape)
 
     # print(rejectedImgPoints)
 
@@ -70,14 +82,17 @@ while(True):
     # my problem was that the cellphone put black all around it. The alrogithm
     # depends very much upon finding rectangular black blobs
 
-    gray = aruco.drawDetectedMarkers(frame, corners, ids)
+    # gray = aruco.drawDetectedMarkers(frame, corners, ids)
     # cv2.polylines(frame, corners[0].astype(np.int32), True, color=(0,255,0))
     # cv2.polylines(frame, [x.reshape(-1,2).astype(np.int32) for x in rejectedImgPoints], True, color=(0,0,255))
-    rvec, tvec, cc = aruco.estimatePoseSingleMarkers(corners, 0.05, camera_matrix, dist_coeffs)
+    # rvec, tvec, cc = aruco.estimatePoseSingleMarkers(corners, 0.05, camera_matrix, dist_coeffs)
     # rvec: rotation vector, tvec: translation vector
-    print(rvec, tvec)
-    print(cc)
-    aruco.drawAxis(frame, camera_matrix, dist_coeffs, rvec, tvec, 0.1)
+    # aruco.drawAxis(frame, camera_matrix, dist_coeffs, rvec, tvec, 0.1)
+
+    homography, mask = cv2.findHomography(src_pts, corners[0])
+    projection = projection_matrix(camera_parameters, homography)
+    
+    frame = render(frame, obj, projection, model, False)
 
     # pt = np.array([0,0,0])
     # # import pdb; pdb.set_trace()
@@ -88,11 +103,9 @@ while(True):
     #print(rejectedImgPoints)
     # Display the resulting frame
     cv2.imshow('frame',frame)
-    cv2.waitKey(0)
-    break
 
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything done, release the capture
 cap.release()
--- main.py
+++ main.py
@@ -7,166 +7,14 @@ if not os.path.isdir('test'):
 
 import pygame
 from OpenGL.GL import *
-
-def MTL(filename):
-    contents = {}
-    mtl = None
-    for line in open(filename, "r"):
-        if line.startswith('#'): continue
-        values = line.split()
-        if not values: continue
-        if values[0] == 'newmtl':
-            mtl = contents[values[1]] = {}
-        elif mtl is None:
-            raise (ValueError, "mtl file doesn't start with newmtl stmt")
-        elif values[0] == 'map_Kd':
-            # load the texture referred to by this declaration
-            mtl[values[0]] = values[1]
-            surf = pygame.image.load(mtl['map_Kd'])
-            image = pygame.image.tostring(surf, 'RGBA', 1)
-            ix, iy = surf.get_rect().size
-            texid = mtl['texture_Kd'] = glGenTextures(1)
-            glBindTexture(GL_TEXTURE_2D, texid)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-                GL_LINEAR)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
-                GL_LINEAR)
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA,
-                GL_UNSIGNED_BYTE, image)
-        else:
-            mtl[values[0]] = map(float, values[1:])
-    return contents
-
-class OBJ:
-    def __init__(self, filename, swapyz=False):
-        """Loads a Wavefront OBJ file. """
-        self.vertices = []
-        self.normals = []
-        self.texcoords = []
-        self.faces = []
-
-        material = None
-        for line in open(filename, "r"):
-            if line.startswith('#'): continue
-            values = line.split()
-            if not values: continue
-            if values[0] == 'v':
-                v = map(float, values[1:4])
-                if swapyz:
-                    v = v[0], v[2], v[1]
-                self.vertices.append(v)
-            elif values[0] == 'vn':
-                v = map(float, values[1:4])
-                if swapyz:
-                    v = v[0], v[2], v[1]
-                self.normals.append(v)
-            elif values[0] == 'vt':
-                self.texcoords.append(map(float, values[1:3]))
-            elif values[0] in ('usemtl', 'usemat'):
-                material = values[1]
-            elif values[0] == 'mtllib':
-                self.mtl = MTL(values[1])
-            elif values[0] == 'f':
-                face = []
-                texcoords = []
-                norms = []
-                for v in values[1:]:
-                    w = v.split('/')
-                    face.append(int(w[0]))
-                    if len(w) >= 2 and len(w[1]) > 0:
-                        texcoords.append(int(w[1]))
-                    else:
-                        texcoords.append(0)
-                    if len(w) >= 3 and len(w[2]) > 0:
-                        norms.append(int(w[2]))
-                    else:
-                        norms.append(0)
-                self.faces.append((face, norms, texcoords, material))
-
-        self.gl_list = glGenLists(1)
-        glNewList(self.gl_list, GL_COMPILE)
-        glEnable(GL_TEXTURE_2D)
-        glFrontFace(GL_CCW)
-        for face in self.faces:
-            vertices, normals, texture_coords, material = face
-
-            mtl = self.mtl[material]
-            if 'texture_Kd' in mtl:
-                # use diffuse texmap
-                glBindTexture(GL_TEXTURE_2D, mtl['texture_Kd'])
-            else:
-                # just use diffuse colour
-                glColor(*mtl['Kd'])
-
-            glBegin(GL_POLYGON)
-            for i in range(len(vertices)):
-                if normals[i] > 0:
-                    glNormal3fv(self.normals[normals[i] - 1])
-                if texture_coords[i] > 0:
-                    glTexCoord2fv(self.texcoords[texture_coords[i] - 1])
-                glVertex3fv(self.vertices[vertices[i] - 1])
-            glEnd()
-        glDisable(GL_TEXTURE_2D)
-        glEndList()
-
-
-def projection_matrix(camera_parameters, homography):
-    """
-    From the camera calibration matrix and the estimated homography
-    compute the 3D projection matrix
-    """
-    # Compute rotation along the x and y axis as well as the translation
-    homography = homography * (-1)
-    rot_and_transl = np.dot(np.linalg.inv(camera_parameters), homography)
-    col_1 = rot_and_transl[:, 0]
-    col_2 = rot_and_transl[:, 1]
-    col_3 = rot_and_transl[:, 2]
-    # normalise vectors
-    l = math.sqrt(np.linalg.norm(col_1, 2) * np.linalg.norm(col_2, 2))
-    rot_1 = col_1 / l
-    rot_2 = col_2 / l
-    translation = col_3 / l
-    # compute the orthonormal basis
-    c = rot_1 + rot_2
-    p = np.cross(rot_1, rot_2)
-    d = np.cross(c, p)
-    rot_1 = np.dot(c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
-    rot_2 = np.dot(c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
-    rot_3 = np.cross(rot_1, rot_2)
-    # finally, compute the 3D projection matrix from the model to the current frame
-    projection = np.stack((rot_1, rot_2, rot_3, translation)).T
-    return np.dot(camera_parameters, projection)
-
-def render(img, obj, projection, model, color=False):
-    vertices = obj.vertices
-    scale_matrix = np.eye(3) * 3
-    h, w = model.shape
-
-    for face in obj.faces:
-        face_vertices = face[0]
-        points = np.array([vertices[vertex - 1] for vertex in face_vertices])
-        points = np.dot(points, scale_matrix)
-        # render model in the middle of the reference surface. To do so,
-        # model points must be displaced
-        points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
-        dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
-        imgpts = np.int32(dst)
-        if color is False:
-            cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
-        else:
-            color = hex_to_rgb(face[-1])
-            color = color[::-1] # reverse
-            cv2.fillConvexPoly(img, imgpts, color)
-
-    return img
-
-
+from model import OBJ
+from utils import projection_matrix, render
 
 obj = OBJ("low-poly-fox-by-pixelmannen.obj", swapyz=True)
 camera_parameters = np.array([[600, 0, 320], [0, 600, 240], [0, 0, 1]])
 MIN_MATCHES = 10
 
-model = cv2.imread('model.png', 0)
+model = cv2.imread('model2.jpg', 0)
 orb = cv2.ORB_create() # ORB keypoint detector
 # create brute force  matcher object
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
@@ -200,7 +48,12 @@ while True:
 
         # compute Homography
         homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-
+        h, w = model.shape
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+        # project corners into frame
+        dst = cv2.perspectiveTransform(pts, homography)  
+        # connect them with lines
+        cap = cv2.polylines(cap, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
         projection = projection_matrix(camera_parameters, homography)
 
         img_rgb = render(img_rgb, obj, projection, model, False)
@@ -208,7 +61,7 @@ while True:
         print("Not enough matches have been found - %d/%d" % (len(matches),
                                                             MIN_MATCHES))
     cv2.imshow('frame', cap)
-    # cv2.imshow('webcam', img_rgb)
+    cv2.imshow('webcam', img_rgb)
     if cv2.waitKey(1) == 27: 
         break  # esc to quit
 cv2.destroyAllWindows()
